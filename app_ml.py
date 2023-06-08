157887e73b013ee4ea32ebd385106b339a1d4eec
5000koc <kocran5000@gmail.com>
2023-06-08 17:08:57 +0900

    Update app_ml.py

--- app_ml.py
+++ app_ml.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def run_app_ml():
     st.subheader('기종간 비교')
 
-    if st.checkbox('PS4 & XBOX 1천만장 판매된 게임'):
+    if st.checkbox('PS4 & XBOX 전세계 1천만장 판매된 게임'):
         fig = plt.figure()
     
         x = ['GTA5','CoD:BO3','RDR2','CoD:WW2','FIFA18','FIFA17','Fallout4','CoD:IW','CoD:AW','BattleField 1','StarWars:BF2015','Minecraft','FIFA16','Unchrted4']
@@ -20,7 +20,7 @@ def run_app_ml():
 
         st.pyplot(fig)
 
-    if st.checkbox('양기종간의 비교'):
+    if st.checkbox('전세계 양기종간의 비교'):
         fig = plt.figure()
 
         data = {'Game':['GTA5','CoD:BO3','RDR2','CoD:WW2','FIFA18','FIFA17','Fallout4','CoD:IW','CoD:AW','BattleField 1','StarWars:BF2015','Minecraft','FIFA16','Unchrted4'],
@@ -40,5 +40,53 @@ def run_app_ml():
 
         st.pyplot(fig)
 
-    
+    if st.checkbox('PS4 & XBOX 북미지역 1천만장 판매 게임'):
+        fig = plt.figure()
 
+        x = ['CoD:BO3','GTA5','RDR2','CoD:WW2','CoD:AW','CoD:IW','Fallout4','StarWars:BF2015','Battlefield 1','Minecraft']
+        y = [10.81,10.76,9.02,8.42,6.09,6.02,5.85,5.8,5.57,5.12]
+
+        plt.bar(x,y,color='orange')
+        plt.title('2013~2020 PS4 & XBOX North America Best Sales Games')
+        plt.xlabel('Game')
+        plt.ylabel('Sales(million)')
+        plt.xticks(rotation = 90)
+
+        st.pyplot(fig)
+
+    if st.checkbox('북미지역 양기종 간 비교'):
+        fig = plt.figure()
+
+        data_us = {'Game':['CoD:BO3','GTA5','RDR2','CoD:WW2','CoD:AW','CoD:IW','Fallout4','StarWars:BF2015','Battlefield 1','Minecraft'],
+        'PS4_sales': [6.18,6.06,5.26,4.67,2.84,3.11,2.91,3.31,2.2,1.89],
+        'Xbox_sales':[4.63,4.7,3.76,3.75,3.25,2.91,2.94,2.49,3.37,3.23]}
+
+        df_total_us = pd.DataFrame(data_us)
+        bar_width = 0.35
+        index = range(len(df_total_us))
+        plt.bar(index, df_total_us['PS4_sales'], width=bar_width, label='PS4')
+        plt.bar([i + bar_width for i in index], df_total_us['Xbox_sales'], width=bar_width, label='XBOX',color='green')
+
+        plt.xlabel('Game')
+        plt.ylabel('Sales (million)')
+        plt.title('PS4 & XBOX North America Best Sales Games Comparison')
+        plt.xticks([i + bar_width/2 for i in index], df_total_us['Game'],rotation = 90)
+        plt.legend()
+
+        st.pyplot(fig)
+
+        if st.checkbox('PS4 & XBOX 유럽지역 최다판매 게임'):
+            fig = plt.figure()
+    
+            x = ['GTA5','FIFA18','FIFA17','CoD:WW2','CoD:BO3','RDR2','FIFA16','FIFA15','Fallout4','CoD:IW']
+            y = [12.96,10.56,10.35,8.12,8.09,7.68,7.67,5.88,5.59,5.27]
+
+            plt.bar(x,y,color='orange')
+            plt.title('2013~2020 PS4 & XBOX Europe Best Sales Games')
+            plt.xlabel('Game')
+            plt.ylabel('Sales(million)')
+            plt.xticks(rotation = 90)
+
+            st.pyplot(fig)
+
+        # if st.checkbox('유럽지역 양기종 간의 비교')
