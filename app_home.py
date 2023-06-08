bc61a1005f81b7a98c034bf61930eacd99f525a0
5000koc <kocran5000@gmail.com>
2023-06-08 17:56:54 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -4,7 +4,7 @@ import streamlit as st
 def run_app_home():
 
     st.subheader('2013~2020 PS4 & XBOX 판매된 게임의 판매량을 볼 수 있습니다')
-    st.text('원본출처는 캐글에 Video Games Sales Dataset 입니다')
+    st.text('원본출처는 캐글의 Video Games Sales Dataset 입니다')
 
     img_url='https://www.expertreviews.co.uk/sites/expertreviews/files/styles/er_main_wide/public/2018/09/xbox_one_vs_ps4_primary.jpg?itok=UsGJtLX-'
     st.image(img_url)
